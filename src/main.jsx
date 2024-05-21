dbe74ab77f9e269622d1125f36ce27d0f7abf81b
Sanjay Sukhwani <sanjayasukhwani@gmail.com>
2024-05-21 18:40:48 +0530

    Added Router in our Application

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,51 @@ import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import store from "./store/store.js"
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import { AddArticle, AllArticles, EditArticle, Home, Login, Signup, ViewArticle } from './pages/index.js'
+import { AuthLayout } from './components/index.js'
+
+// Now, Adding the BrowserRoutes
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path='/' element={<App />} >
+            <Route path='' element={<Home />} />
+            <Route path='/login' element={
+                <AuthLayout authentication={false}>
+                    <Login />
+                </AuthLayout>
+            } />
+            <Route path='/signup' element={
+                <AuthLayout authentication={false}>
+                    <Signup />
+                </AuthLayout>
+            } />
+            <Route path='/all-articles' element={
+                <AuthLayout authentication={true}>
+                    <AllArticles />
+                </AuthLayout>
+            } />
+            <Route path='/add-article' element={
+                <AuthLayout authentication={true}>
+                    <AddArticle />
+                </AuthLayout>
+            } />
+            <Route path='/edit-article/:slug' element={
+                <AuthLayout authentication={true}>
+                    <EditArticle />
+                </AuthLayout>
+            } />
+            <Route path='/article/:slug' element={
+                <ViewArticle />
+            } />
+        </Route>
+    )
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <Provider store={store}>
-            <App />
+            <RouterProvider router={router} />
         </Provider>
     </React.StrictMode>,
 )
